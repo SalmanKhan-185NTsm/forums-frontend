5b18f7ec180be2634b83f730c0c1a940d5c688ce
SalmanKhan-185NTsm <salmankhans185@gmail.com>
2025-02-20 16:16:35 +0530

    edit form update

--- src/app/components/Posts/EditPostForm.tsx
+++ src/app/components/Posts/EditPostForm.tsx
@@ -187,7 +187,7 @@ export default function EditPostForm({ userSession, postData }: Props) {
           </button>
           <Link
             type="Cancel"
-            href={`/posts/view-details/${""}`}
+            href={`/posts/view-details/${postData.postId}`}
             className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2"
           >
             Cancel
